cef9f6d418e2f6471e0bab3ef7e0f5114f9f8dab
GAURAB <gaurabkhadka4082@gmail.com>
2024-05-03 18:40:40 +0545

    Question no 3

--- Week2/Q3.java
+++ Week2/Q3.java
@@ -1,6 +1,6 @@
 package Week2;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Q3 {
     //Calculate, area of the triangle, and the Volume of the Cube and Cuboid. 
@@ -12,5 +12,26 @@ public class Q3 {
         int height = triangle.nextInt();
         float area = (base*height)/2;
         System.out.println("The area of tirangle is: "+area); 
+        triangle.close();
+
+        // Volume of cube
+        Scanner cube = new Scanner(System.in);
+        System.out.print("Enter length:");
+        int length = cube.nextInt();
+        int volumeCube = length*length*length;
+        System.err.println("Volume of cube is "+volumeCube);
+        cube.close();
+
+        //Volume of cuboid
+        Scanner cuboid = new Scanner(System.in);
+        System.out.print("Enter length of cuboid: ");
+        int len = cuboid.nextInt();
+        System.out.print("Enter breadth of cuboid: ");
+        int breadth = cuboid.nextInt();
+        System.out.print("Enter height of cuboid: ");
+        int heightCuboid = cuboid.nextInt();
+        int volumeCuboid = len*breadth*heightCuboid;
+        System.out.println("Volume of cuboid is "+ volumeCuboid);
+        cuboid.close();
     }   
 }
