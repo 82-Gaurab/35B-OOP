f8f694f643d0ebcead9bf8513c3ae2ede3871409
GAURAB <gaurabkhadka4082@gmail.com>
2024-05-06 07:26:20 +0545

    Week3 Q2

---
+++ Week3/Q2.java
@@ -0,0 +1,24 @@
+package Week3;
+
+import java.util.Scanner;
+
+public class Q2 {
+    /* check whether a number is negative, positive, or zero. */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scan.nextInt();
+        scan.close();
+        if (number>0) {
+            System.out.println("The given number is positive.");
+        }
+        else if (number<0) {
+            System.out.println("The given number is negative.");
+        }
+        else{
+            System.out.println("The given number is zero.");
+        }
+
+
+    }
+}
