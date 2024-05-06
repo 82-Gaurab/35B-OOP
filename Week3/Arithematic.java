9b1cab1556597291ae71a6a302e4d718822c2e4d
GAURAB <gaurabkhadka4082@gmail.com>
2024-05-06 09:24:50 +0545

    formatting the file

--- Week3/Arithematic.java
+++ Week3/Arithematic.java
@@ -3,9 +3,12 @@ package Week3;
 import java.util.Scanner;
 
 public class Arithematic {
-    /*takes two numbers and an operator (+, -, *, /) as inputs 
-    and performs the corresponding arithmetic operation using a switch case statement */
-    public static void main(String[] args){
+    /*
+     * takes two numbers and an operator (+, -, *, /) as inputs
+     * and performs the corresponding arithmetic operation using a switch case
+     * statement
+     */
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter two numbers: ");
         int num1 = scan.nextInt();
@@ -14,32 +17,31 @@ public class Arithematic {
         String operator = scan.next();
 
         scan.close();
-        
+
         switch (operator) {
             case "+":
-                int sum = num1+num2;
-                System.out.printf("%d + %d = %d",num1,num2,sum);
+                int sum = num1 + num2;
+                System.out.printf("%d + %d = %d", num1, num2, sum);
                 break;
 
             case "-":
-                int difference  = num1 - num2;
-                System.out.printf("%d - %d = %d",num1,num2,difference);
+                int difference = num1 - num2;
+                System.out.printf("%d - %d = %d", num1, num2, difference);
                 break;
-            
+
             case "*":
-                System.out.printf("%d * %d = %d",num1,num2,num1*num2);
+                System.out.printf("%d * %d = %d", num1, num2, num1 * num2);
                 break;
 
             case "/":
-                float div = num1/num2;
-                System.out.printf("%d / %d = %d",num1,num2,div);
+                float div = num1 / num2;
+                System.out.printf("%d / %d = %d", num1, num2, div);
                 break;
-        
+
             default:
                 System.out.println("Not a valid arithematic operator");
                 break;
         }
 
-        
     }
 }
