2760723f838f3c2545d6b09539a075f4dfcf0bdf
GAURAB <gaurabkhadka4082@gmail.com>
2024-05-10 15:13:32 +0545

    Switch Case Q3

---
+++ Week3/Season.java
@@ -0,0 +1,19 @@
+package Week3;
+
+import java.util.Scanner;
+
+public class Season {
+    /*
+     * takes an integer input (1 to 12) representing a month and prints the
+     * corresponding season (e.g., 1-3: Winter, 4-6: Spring, 7-9: Summer, 10-12:
+     * Fall) using a switch case
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number representing a month: ");
+        int month = scan.nextInt();
+
+
+
+    }
+}   
