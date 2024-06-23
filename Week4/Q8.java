26576b9fc3d8a0c4ff00b9e6bac3dbdfbcd08b95
GAURAB <gaurabkhadka4082@gmail.com>
2024-06-23 13:51:01 +0545

    Q8

---
+++ Week4/Q8.java
@@ -0,0 +1,22 @@
+package Week4;
+
+import java.util.Scanner;
+
+public class Q8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        String stnum = scan.nextLine();
+        char[]  rev= new char[stnum.length()];
+        
+        int j=0;
+        for (int i = stnum.length() - 1; i >= 0; i--) {
+            rev[j] = stnum.charAt(i);
+            j++;
+        }
+        for(char i: rev){
+            System.out.print(i);
+        }
+
+    }
+}
