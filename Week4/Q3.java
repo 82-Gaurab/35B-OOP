28dc9f5848622d5de03fea3d0b0d40cae7ec4db2
GAURAB <gaurabkhadka4082@gmail.com>
2024-06-23 13:31:03 +0545

    Q3

---
+++ Week4/Q3.java
@@ -0,0 +1,10 @@
+package Week4;
+
+public class Q3 {
+    public static void main(String[] args) {
+        System.out.println("Num                 Squared");
+        for(int i=10;i>0;i--){
+            System.out.println(i+"                   "+i*i);
+        }
+    }
+}
