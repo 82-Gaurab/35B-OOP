0394844a8b146988d12928c5983dcb33416c0c90
GAURAB <gaurabkhadka4082@gmail.com>
2024-06-23 13:34:12 +0545

    Q4

---
+++ Week4/Q4.java
@@ -0,0 +1,17 @@
+package Week4;
+
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter width");
+        int width = scan.nextInt();
+        for(int i = 0; i<width;i++){
+            for(int j=0; j<width; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
