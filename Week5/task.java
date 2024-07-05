8c0ddcf110f925b37ac8913327708322234cc83d
GAURAB <gaurabkhadka4082@gmail.com>
2024-07-05 18:34:22 +0545

    question no 2

--- Week5/task.java
+++ Week5/task.java
@@ -1,24 +1,33 @@
 // Package Week5;
 
-public class task{
+public class task {
 
     // 1. Make a method to check if a number is prime or not.
-    public static void prime(int num){
-        int count =0;
-        int i=1;
-        while (num % i ==0) {
+    public static void prime(int num) {
+        int count = 0;
+        int i = 1;
+        while (num % i == 0) {
             count++;
-            i++;           
+            i++;
         }
-        if (count >1){
-            System.out.printf("%d is not prime",num);
+        if (count > 1) {
+            System.out.printf("%d is not prime\n", num);
+        } else {
+            System.out.printf("%d is prime\n", num);
         }
-        else{
-            System.out.printf("%d is prime",num);
+    }
+
+    // 2. Make a method to check if a given number n is even or not.
+    public static void even(int n) {
+        if (n % 2 == 0) {
+            System.out.printf("%d is even\n", n);
+        } else {
+            System.out.printf("%d is odd\n", n);
         }
     }
 
     public static void main(String[] args) {
-        prime(13);
+        prime(12);
+        even(7);
     }
 }
