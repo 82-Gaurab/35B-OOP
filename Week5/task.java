fa7df4b43e9f564192566efcbffd413de14bd985
GAURAB <gaurabkhadka4082@gmail.com>
2024-07-06 10:08:19 +0545

    question 8: Fibonacci number

--- Week5/task.java
+++ Week5/task.java
@@ -27,46 +27,69 @@ public class task {
     }
 
     // 3. Make a method to print the table of a given number n.
-    public static void table(int n){
-        for(int i = 1; i <=10;i++){
-            System.out.printf("%d X %d = %d\n",n,i,n*i );
+    public static void table(int n) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d X %d = %d\n", n, i, n * i);
         }
     }
 
-    // 4. Write a program for multiplying 3 numbers using a method and return the result and print the result from the main method.
-    public static int multliplication(int num1,int num2, int num3){
-        return num1*num2*num3;
+    // 4. Write a program for multiplying 3 numbers using a method and return the
+    // result and print the result from the main method.
+    public static int multliplication(int num1, int num2, int num3) {
+        return num1 * num2 * num3;
     }
 
-    // 5. Write a program for calculating Simple Interest using a method and print the result from the method itself.
-    public static void SI(int principle, int rate, int time){
-        double simpleInterest = (principle*rate*time)/100;
-        System.out.printf("Simple Interest = %f\n",simpleInterest);
+    // 5. Write a program for calculating Simple Interest using a method and print
+    // the result from the method itself.
+    public static void SI(int principle, int rate, int time) {
+        double simpleInterest = (principle * rate * time) / 100;
+        System.out.printf("Simple Interest = %f\n", simpleInterest);
     }
 
-    // 6. Write a program to calculate the area of a rectangle using a method of your choice.
-    public static int area(int length, int breadth){
-        return length*breadth;
+    // 6. Write a program to calculate the area of a rectangle using a method of
+    // your choice.
+    public static int area(int length, int breadth) {
+        return length * breadth;
     }
 
     // 7. Calculates the factorial of a given number using the Java method.
-    public static int factorial(int n ){
-        if (n==0){
+    public static int factorial(int n) {
+        if (n == 0) {
             return 1;
-        }
-        else{
-            return n*factorial(n-1);
+        } else {
+            return n * factorial(n - 1);
         }
     }
 
+    // 8. Calculate the nth Fibonacci number using the method.
+    public static void Fibonacci(int n) {
+        if (n == 1) {
+            System.out.println("Fibonacci = 0");
+            return;
+        } else if (n == 2) {
+            System.out.println("Fibonacci = 1");
+            return;
+        }
+
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        System.out.println("Fibonacci = " + c);
+    }
 
     public static void main(String[] args) {
         prime(12);
         even(7);
         table(5);
-        System.out.println("The multliplication = "+ multliplication(3, 4, 10));
+        System.out.println("The multliplication = " + multliplication(3, 4, 10));
         SI(1000, 4, 5);
-        System.out.println("Area of rectangle = "+ area(25,15));
-        System.out.println("Factorial = "+factorial(7));
+        System.out.println("Area of rectangle = " + area(25, 15));
+        System.out.println("Factorial = " + factorial(2));
+        Fibonacci(6);
     }
 }
