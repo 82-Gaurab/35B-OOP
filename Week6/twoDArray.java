ae8f94362696042f5809bc3b3896135a87baa4c0
GAURAB <gaurabkhadka4082@gmail.com>
2024-07-12 14:15:39 +0545

    Q2

--- Week6/twoDArray.java
+++ Week6/twoDArray.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class twoDArray {
     public static void main(String[] args) {
         // 1. Write a java program to Add two 2x3 matrices
-        int[][] m1 = {{1,2,3},{5,6,7}};
-        int[][] m2 = {{1,2,3},{1,2,3}};
+        int[][] m1 = { { 1, 2, 3 }, { 5, 6, 7 } };
+        int[][] m2 = { { 1, 2, 3 }, { 1, 2, 3 } };
 
         int[][] sum = new int[2][3];
         for (int i = 0; i < 2; i++) {
@@ -15,5 +15,16 @@ public class twoDArray {
             }
         }
         System.out.println(Arrays.deepToString(sum));
+
+        // 2. Write a java program to print transpose 2X2 matrices
+        int[][] m3 = { { 1, 2 }, { 3, 4 } };
+        int[][] transpose = new int[2][2];
+        System.out.println("Original matrix: " + Arrays.deepToString(m3));
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                transpose[i][j] = m3[j][i];
+            }
+        }
+        System.out.println("Transpose : " + Arrays.deepToString(transpose));
     }
 }
