26f6e8872ab9361e0e5d5b41298b7caba98a8cb5
GAURAB <gaurabkhadka4082@gmail.com>
2024-07-12 15:02:30 +0545

    Q2

--- Week6/arrayListTask.java
+++ Week6/arrayListTask.java
@@ -1,5 +1,7 @@
 package Week6;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class arrayListTask {
     public static void main(String[] args) {
@@ -13,5 +15,26 @@ public class arrayListTask {
         cars.add("Ford");
 
         System.out.println(cars);
+
+        // 2. WAP to sort the integer elements of array of size 10 in ascending and
+        // descending order using ArrayList.
+        ArrayList<Integer> element = new ArrayList<>();
+        Collections.addAll(element, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        int n = element.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (element.get(j) > element.get(j + 1)) {
+                    // Swap element[j] and element[j+1]
+                    int temp = element.get(j);
+                    element.set(j, element.get(j + 1));
+                    element.set(j + 1, temp);
+                }
+            }
+        }
+        System.out.println("In Ascending Order:\n" + element);
+
+        Collections.sort(element, Collections.reverseOrder());
+        System.out.println("In Descending Order:\n" + element);
+
     }
 }
