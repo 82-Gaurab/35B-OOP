09a6e8a6fca96d5a4c0f25f4558d7e6663dd06c8
GAURAB <gaurabkhadka4082@gmail.com>
2024-07-12 14:23:29 +0545

    Q4

--- Week6/twoDArray.java
+++ Week6/twoDArray.java
@@ -35,5 +35,32 @@ public class twoDArray {
             mirroredMatrix[i][1] = m3[i][0];
         }
         System.out.println("Mirrored matirx:\n"+Arrays.deepToString(mirroredMatrix));
+
+        // 4. Write a java program to Multiply two 2x3 matrices
+        int[][] matrix1 = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int[][] matrix2 = {
+            {7, 8},
+            {9, 10},
+            {11, 12}
+        };
+
+        int rows1 = matrix1.length;
+        int cols1 = matrix1[0].length;
+        int cols2 = matrix2[0].length;
+
+        int[][] resultMatrix = new int[rows1][cols2];
+
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols2; j++) {
+                for (int k = 0; k < cols1; k++) {
+                    resultMatrix[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        System.out.println("Multliplication:\n"+ Arrays.deepToString(resultMatrix));
     }
 }
