88f623454143caff784e7171540c05f7a8931421
GAURAB <gaurabkhadka4082@gmail.com>
2024-07-13 15:25:24 +0545

    Q5: abstract clas Draw

--- Week10/Main.java
+++ Week10/Main.java
@@ -23,6 +23,22 @@ public class Main {
         Motorcycle motorcycle = new Motorcycle();
         motorcycle.startEngine();
         motorcycle.stopEngine();
+        System.out.println();
+
+        Cuboid cuboid = new Cuboid(10, 6, 5);
+        System.out.println("The Area of cuboid: "+cuboid.calculateArea());
+        System.out.println("The Perimeter of cuboid: "+cuboid.calculatePerimeter());
+        System.out.println("The volume of cuboid: "+cuboid.calculateVolume());
+
+        Cube cube = new Cube(5);
+        System.out.println("The area of cube: "+cube.calculateArea());
+        System.out.println("The Perimeter of cube: "+cube.calculatePerimeter());
+        System.out.println("The Voulume of cube: "+cube.calculateVolume());
+
+        Cylinder cylinder = new Cylinder(7, 10);
+        System.out.println("The area of cylinder: "+cylinder.calculateArea());
+        System.out.println("The Perimeter of cylinder: "+cylinder.calculatePerimeter());
+        System.out.println("The Voulume of cylinder: "+cylinder.calculateVolume());
 
     }
 }
@@ -100,12 +116,13 @@ class Circle extends Shape {
 // stopEngine() methods in both subclasses to start and stop the engines of a
 // car and a motorcycle, respectively.
 
-abstract class Vehicle{
+abstract class Vehicle {
     abstract void startEngine();
+
     abstract void stopEngine();
 }
 
-class Car extends Vehicle{
+class Car extends Vehicle {
 
     @Override
     void startEngine() {
@@ -119,7 +136,7 @@ class Car extends Vehicle{
 
 }
 
-class Motorcycle extends Vehicle{
+class Motorcycle extends Vehicle {
 
     @Override
     void startEngine() {
@@ -131,4 +148,96 @@ class Motorcycle extends Vehicle{
         System.out.println("Motorcycle engine has stopped");
     }
 
+}
+
+// Q5. Abstract class Draw and Create subclasses for different shapes such as
+// "Cube," "Cuboid," and "Cylinder."
+
+abstract class Draw {
+    abstract double calculateVolume();
+
+    abstract double calculateArea();
+
+    abstract double calculatePerimeter();
+}
+
+class Cuboid extends Draw{
+
+    int length;
+    int breadth;
+    int height;
+
+    Cuboid(int length, int breadth, int height){
+        this.length = length;
+        this.breadth = breadth;
+        this.height = height;
+    }
+
+    @Override
+    double calculateVolume() {
+        return length*breadth*height;
+    }
+
+    @Override
+    double calculateArea() {
+        return 	2*(length*breadth + breadth*height + height*length);
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return 4 * (length+breadth+height);
+    }
+    
+}
+
+class Cube extends Draw{
+
+    int length;
+
+    Cube(int length){
+        this.length = length;
+    }
+
+    @Override
+    double calculateVolume() {
+        return length*length*length;
+    }
+
+    @Override
+    double calculateArea() {
+        return 6*length*length;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return 6*length;
+    }
+
+}
+
+class Cylinder extends Draw{
+
+    int radius;
+    int height;
+
+    Cylinder(int radius, int height){
+        this.radius = radius;
+        this.height = height;
+    }
+
+    @Override
+    double calculateVolume() {
+        return Math.PI*radius*radius*height;
+    }
+
+    @Override
+    double calculateArea() {
+        return 2* Math.PI *radius *height + 2* Math.PI *radius*radius;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return (4*radius + 2*height);
+    }
+    
 }
