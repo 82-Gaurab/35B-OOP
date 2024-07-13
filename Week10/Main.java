56610e119fae8d7447759acc04b27df508451347
GAURAB <gaurabkhadka4082@gmail.com>
2024-07-13 14:54:34 +0545

    Q2: abstract class Shape

--- Week10/Main.java
+++ Week10/Main.java
@@ -7,6 +7,14 @@ public class Main {
 
         Cat cat = new Cat();
         cat.sound();
+        System.out.println();
+
+        Rectangle rectangle = new Rectangle(4, 10);
+        System.out.println("Area of rectangle: " + rectangle.calculateArea());
+
+        Circle circle = new Circle(10);
+        System.out.println("Area of circle: " + circle.calculateArea());
+
     }
 }
 
@@ -36,4 +44,43 @@ class Cat extends Animal {
         System.out.println("Meow");
     }
 
+}
+
+// 2. Create an abstract class called Shape with an abstract method called
+// calculateArea(). Implement two subclasses called Rectangle and Circle that
+// inherit from Shape. Override the calculateArea() method in both subclasses to
+// calculate and return the area of a rectangle and a circle, respectively.
+// Write the Java code to implement this scenario with Scanner Input.
+
+abstract class Shape {
+    abstract double calculateArea();
+}
+
+class Rectangle extends Shape {
+    private int length;
+    private int breadth;
+
+    Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    @Override
+    double calculateArea() {
+        return length * breadth;
+    }
+
+}
+
+class Circle extends Shape {
+    private int radius;
+
+    Circle(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double calculateArea() {
+        return Math.PI * 2 * radius;
+    }
 }
