8113602b3e789f7ea7cefc98913d39423e0dba23
GAURAB <gaurabkhadka4082@gmail.com>
2024-07-13 12:52:23 +0545

    Q2: override method

--- Week9/Main.java
+++ Week9/Main.java
@@ -2,7 +2,13 @@ package Week9;
 
 public class Main {
     public static void main(String[] args) {
+        // Instance of Car
         Car c = new Car("Ford", "Mustang", "White", 2020);
         c.display();
+        System.out.println();
+
+        // Instance of HR Manager
+        HRManager hr = new HRManager();
+        hr.work();
     }
 }
